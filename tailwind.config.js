6f88a3d7227d2fef470804923fba12694e0bbe98
Hendry Lim <myhendry@gmail.com>
2020-11-08 14:05:12 +0800

    initial commit

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  future: {
+    removeDeprecatedGapUtilities: true,
+    purgeLayersByDefault: true,
+  },
+  purge: [],
+  theme: {
+    extend: {
+      fontFamily: {
+        //        body: ["Turret Road:200,300,400,500,700,800"],
+        body: ["Turret Road"],
+      },
+    },
+  },
+  variants: {},
+  plugins: [],
+};
