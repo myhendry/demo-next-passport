61263bfb691240e3e4fc847a766f12750f3845b5
Hendry Lim <myhendry@gmail.com>
2020-11-14 09:04:53 +0800

    added mongodb

--- pages/api/hello.js
+++ pages/api/hello.js
@@ -1,6 +1,20 @@
-// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import nextConnect from "next-connect";
 
-export default (req, res) => {
-  res.statusCode = 200
-  res.json({ name: 'John Doe' })
-}
+import middleware from "../../middlewares/middleware";
+
+const handler = nextConnect();
+
+handler.use(middleware);
+
+handler.get(async (req, res) => {
+  const user = await req.db
+    .collection("users")
+    .insertOne({
+      email: "ohendry@email.com",
+    })
+    .then(({ ops }) => ops[0]);
+  console.log(user);
+  res.status(201).send("Done");
+});
+
+export default handler;
